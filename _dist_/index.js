8db99159232935a93e5f292dda661feca5c9d5b9
Mooenz <Mooenz@users.noreply.github.com>
2021-09-25 03:10:56 +0000

    deploy: 7773ba42584c8ff1a8fb302bdeea160c5dc53e22

---
+++ _dist_/index.js
@@ -0,0 +1,49 @@
+/**
+ * This file is just a silly example to show everything working in the browser.
+ * When you're ready to start on your site, clear the file. Happy hacking!
+ **/
+import { registerImage } from "./lazy.js";
+
+const CONTAINER_IMAGES = document.querySelector(".images");
+const CONTAINER_CONTENT = document.querySelector(".container_content");
+
+const MAX = 122;
+const MIN = 1;
+
+const numberRandom = () => Math.floor(Math.random() * (MIN - MAX) + MAX);
+
+
+function renderImages() {
+  const contenedor = document.createElement("div");
+  contenedor.classList = "m-4 bg-gray-300";
+  contenedor.width = 400;
+  contenedor.height = 300;
+
+  const image = document.createElement("img");
+  image.dataset.src = `https://randomfox.ca/images/${numberRandom()}.jpg`;
+  image.classList = "mx-auto";
+  image.alt = "img random";
+  image.width = 400;
+  image.height = 300;
+
+
+  contenedor.append(image);
+
+  registerImage(image);
+
+  createImage += 1;
+  showMessage();
+  
+  CONTAINER_IMAGES.append(contenedor);
+}
+
+function click(ev) {
+  const target = ev.target.id;
+  if (target === "add") {
+    renderImages();
+  } else if (target === "clear") {
+    console.log(target);
+  }
+}
+
+CONTAINER_CONTENT.addEventListener("click", click);
